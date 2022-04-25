c3e01038cc898422a346e1d63bab0782a93ff306
veronica-hing <veronica.m.hing@gmail.com>
2022-04-25 16:52:18 -0700

    created functions to create default pizza, user defined pizza and random pizza

---
+++ dojo-pizzeria.js
@@ -0,0 +1,38 @@
+function pizzaOven(crustType = "thin crust", sauceType = "marinara", cheeses = ["mozzarella", "asiago", "parmesan"], toppings = ["tomato", "basil"]){
+    var pizza = {};
+    pizza.crust = crustType;
+    pizza.sauce = sauceType;
+    pizza.cheeses = cheeses;
+    pizza.toppings = toppings;
+    return pizza;
+}
+
+//returns a pizza using default values
+console.log(pizzaOven());
+
+//returns a pizza with deep dish crust, traditional sace, mozzarella cheese, and pepperoni and sausage toppings
+console.log(pizzaOven("deep dish", "traditional", ["mozzarella"], ["pepperoni", "sausage"]));
+
+//hand tossed, marinara, mozzarella + feta, mushrooms, olives onions pizza
+console.log(pizzaOven("hand-tossed", "marinara",["mozzarella", "feta"], ["mushrooms","olives","onions"]));
+
+//2 more pizzas with any toppings, going with default then hawaiian
+console.log(pizzaOven());//default pizza
+console.log(pizzaOven("thin crust", "marinara", ["mozzarella"], ["pineapple", "chicken", "bacon"]));
+
+function randomPizza(){
+    const seed = Math.random();
+    const seed2 = Math.random();
+    const allToppings = ["chicken", "tomatoes", "artichokes", "anchovies","pineapple", "onions","bell peppers", "ghost peppers", "olives", "shroomies", "bacon", "pepperoni", "sausage", "garlic"];
+    const allSauces = ["pesto", "marinara", "alfredo"];
+    const allCrusts = ["thin crust", "deep dish", "thick crust", "filled crust"];
+    const allCheeses = ["mozzarella", "parmesan", "asiago", "brie"];
+    var myCrust = allCrusts[Math.floor(seed*allCrusts.length)];
+    var mySauce = allSauces[Math.floor(seed*allSauces.length)];
+    var myCheeses = [allCheeses[Math.floor(seed*allCheeses.length)], allCheeses[Math.floor(seed2*allCheeses.length)]];
+    var myToppings = [allToppings[Math.floor(seed*allToppings.length)], allToppings[Math.floor(seed2*allToppings.length)]];
+    return pizzaOven(myCrust, mySauce, myCheeses, myToppings);
+}
+
+console.log("random pizza")
+console.log(randomPizza());
