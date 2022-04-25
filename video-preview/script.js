e13af922d4c05734e24003774b0f52a355e0d18e
veronica-hing <veronica.m.hing@gmail.com>
2022-04-25 14:27:04 -0700

    added video to assets and enabled muted autoplay for video. User must interact with page first before hovering does autoplay stuff

--- video-preview/script.js
+++ video-preview/script.js
@@ -1,1 +1,13 @@
 console.log("page loaded...");
+
+function startMutePlay(elem){
+    elem.setAttribute("muted","true");
+    //elem.setAttribute("autoplay","true");
+    elem.play();
+}
+
+function stopMutePlay(elem){
+    elem.removeAttribute("muted");
+    //elem.removeAttribute("autoplay");
+    elem.pause();
+}
